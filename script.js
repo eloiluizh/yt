4f59852ca63d01d33239b4131191725c014c4fa5
Eloi Luiz Hegner <120321856+eloiluizh@users.noreply.github.com>
2024-09-19 18:36:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function fetchVideos() {
     const apiKey = 'AIzaSyDxEJJAhX0Mb6X_pJceC2kSCxOmVv0Gi7w'; // Substitua com sua chave de API
-    const allowedChannels = ['Channel1', 'Channel2'];
-    const query = 'your search query';
+    const allowedChannels = ['Jazzghost', 'Channel2'];
+    const query = 'jazzghost';
     const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&key=${apiKey}`);
     const data = await response.json();
     const videos = data.items.filter(item => allowedChannels.includes(item.snippet.channelTitle));
